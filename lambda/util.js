e57f3ba49627b194e1a62ee577dd41edf61741e9
Lukas Kiffmeyer <l.kiffmeyer@fh-muenster.de>
2026-05-24 15:03:58 +0200

    Setze Codebase auf funktionierende Version zurueck und migriere AWS SDK auf v3 zur Behebung von Sicherheitsluecken

--- lambda/util.js
+++ lambda/util.js
@@ -1,17 +1,20 @@
-const AWS = require('aws-sdk');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
-const s3SigV4Client = new AWS.S3({
-    signatureVersion: 'v4',
+const s3Client = new S3Client({
     region: process.env.S3_PERSISTENCE_REGION
 });
 
-module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
+module.exports.getS3PreSignedUrl = async function getS3PreSignedUrl(s3ObjectKey) {
 
     const bucketName = process.env.S3_PERSISTENCE_BUCKET;
-    const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
+    const command = new GetObjectCommand({
         Bucket: bucketName,
         Key: s3ObjectKey,
-        Expires: 60*1 // the Expires is capped for 1 minute
+    });
+
+    const s3PreSignedUrl = await getSignedUrl(s3Client, command, {
+        expiresIn: 60 // 60 seconds (1 minute)
     });
     console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
     return s3PreSignedUrl;
